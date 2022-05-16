1ac3774f095e7fe90e124b25abbf84072ddcf704
chantalgenoud <chantal.genoud@gmail.com>
2022-05-16 16:17:16 +0200

    day 1 ende

--- welcome-screen/src/App.vue
+++ welcome-screen/src/App.vue
@@ -1,24 +1,48 @@
-<template>
-  <div id="app">
-    <h1>LOREM IPSUM DOLOR</h1>
-    <span> </span>
 
-    <ul>
-      <li>
-        <span> <!-- daytime --> </span>
-        <h3><!-- title --></h3>
-        <span> <!-- description --> </span>
-      </li>
-    </ul>
+
+<template>
+
+    <div id="app">
+    <h1>Welcome to Opportunity</h1>
+    <!--<span> </span> -->
+
+    <div class="blauesRechteck">
+      <ul>
+          <li>
+          <span style=color:#EB5E00> 14.00 Uhr </span>
+          <h3>Basisbeschäftigung Besuch</h3>
+          <span>Interessierte für den zweiten Kurs werden uns besuchen</span>
+        </li>
+      </ul>
+        </div>
+
+          <ul>
+        <li>
+          <span> <!-- daytime --> </span>
+          <h3><!-- title --></h3>
+          <span> <!-- description --> </span>
+        </li>
+      </ul>
+
+          <ul>
+        <li>
+          <span> <!-- daytime --> </span>
+          <h3><!-- title --></h3>
+          <span> <!-- description --> </span>
+        </li>
+      </ul>
+
 
     <footer>
-      <div style: width="1080p" height="130px"> <!--not sure -->
+      <div> <!--not sure style: width="1080p" height="130px" -->
         <img src="./assets/STZH_SEB_Logo.png" width="230px" height="44px">
         <img src="./assets/Opportunity.png" width="296px" height="55px">
         <img src="./assets/SAG_Logo_De.png" width="273px" height="52px">
        </div> 
     </footer>
+
   </div>
+
 </template>
 
 <script>
@@ -28,17 +52,39 @@ export default {
 </script>
 
 <style>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto, sans-serif';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
 
 body {
   background-color: #E5E5E5;
+  text-align: left;
 }
 
+
+
+h1 {
+  text-align: left;
+  font-family: 'Roboto', sans-serif;
+  font-size: 62px;
+  font-weight: 90;
+}
+
+.blauesRechteck {
+  width: 960px;
+  height: 182px;
+  position: absolute;
+  padding-right: -4.46%;
+  background: #0f05a0;
+
+}
 </style>
