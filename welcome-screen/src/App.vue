64896295b28bfe8a0f615d5bb5a3f1963ad6b0de
chantalgenoud <chantal.genoud@gmail.com>
2022-05-16 14:39:21 +0200

    set up

---
+++ welcome-screen/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>LOREM IPSUM DOLOR</h1>
+    <span> </span>
+
+    <ul>
+      <li>
+        <span> <!-- daytime --> </span>
+        <h3><!-- title --></h3>
+        <span> <!-- description --> </span>
+      </li>
+    </ul>
+
+    <footer>
+      <div style: width="1080p" height="130px"> <!--not sure -->
+        <img src="./assets/STZH_SEB_Logo.png" width="230px" height="44px">
+        <img src="./assets/Opportunity.png" width="296px" height="55px">
+        <img src="./assets/SAG_Logo_De.png" width="273px" height="52px">
+       </div> 
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  background-color: #E5E5E5;
+}
+
+</style>
